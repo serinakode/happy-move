04b7b94367e0a94082146594ebcfca880734503a
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-14 17:55:08 +1000

    add handleChange & handleSubmit

--- src/js/components/Options.js
+++ src/js/components/Options.js
@@ -1,22 +1,46 @@
 import React from 'react'
 import { Option } from './Option'
 
-const Options = (props) => {
+class Options extends React.Component {
 
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Remove All</button>
-            {props.options.length === 0 && <p>Add your dreams</p>}
-                {props.options.map = (option) => { 
-                    <Option
+    state = {
+        value: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+
+    }
+
+    handleSubmit = (event) => {
+        this.props.handleAddOption(this.state.value)
+        event.preventDefault()
+        // when we work on form, use preventDefault()
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+                    </label>
+                    <input type="submit" value="Add" />
+                </form>
+                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
+                {this.props.options.length === 0 && <p>Add your dreams</p>}
+                {this.props.options.map((option) => {
+                   
+                   return <Option
                         key={option}
                         optionText={option}
-                        handleDeleteOption={props.handleDeleteOption}
+                        handleDeleteOption={this.props.handleDeleteOption}
                     />
-                    }
+                })
                 }
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
 export { Options }
