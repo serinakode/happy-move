2b1bc28eba1647c87d4f478cd5917be2ce59152d
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-14 17:57:11 +1000

    add logic to save data in localStorage and load them

--- src/js/components/AddOption.js
+++
@@ -1,7 +0,0 @@
-// import React from 'react'
-
-// const AddOption = () => {
-    
-// }
-
-// export { AddOption }
--- src/js/components/Body.js
+++ src/js/components/Body.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import { Options } from './Options';
-import { AddOption } from './AddOption';
 
 class Body extends React.Component {
     state = {
         option: null,
-        options: []
+        options: [] 
+    }
+
+    // whenever the body component gets mounted/inserted onto the page
+    // to get some data from the local storage
+    componentDidMount() {
+        this.handleLoad()
+    }
+
+    // whenever the props/state of the component changes 
+    componentDidUpdate() {
+            this.handleSave()
+    }
+    
+     // when we refresh the page, previous entries would still remain.
+    handleSave() {
+        const result = JSON.stringify(this.state.options)
+        localStorage.setItem('options', result);
+    }
+
+    // load data first and convert it from JSON data to JS object.
+    // showing what's stored in the localstorage 
+    handleLoad() {
+        const result = localStorage.getItem('options')
+
+        this.setState(() => {
+            return {
+                options: JSON.parse(result)
+            }
+        })
     }
 
     handleDeleteOptions = (options) => {
@@ -22,11 +50,28 @@ class Body extends React.Component {
         }));
     }
 
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add your dream'
+        } else if (this.state.options.indexOf(option) > -1) {
+            alert ( 'This option already exists' )
+        }
+            
+        // this.setState((prevState) => ({
+        //     options: prevState.options.concat(option)
+        // })
+
+        this.setState((prevState) => {
+            return { 
+                options: prevState.options.concat(option)
+            }
+        })
+    }
+
     render() {
         return (
         <div>
-            <Options options={this.state.options} handleDeleteOptions={this.handleDeleteOptions} handleDeleteOption={this.handleDeleteOption} />
-            {/* <AddOption /> */}
+            <Options options={this.state.options} handleAddOption={this.handleAddOption} handleDeleteOptions={this.handleDeleteOptions} handleDeleteOption={this.handleDeleteOption} />
         </div>   
         )
     }
